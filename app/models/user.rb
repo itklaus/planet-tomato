a842023a7d7342ca597c22710e35ac3aab07eb31
itklaus <it.thomas@yandex.ru>
2017-09-24 18:16:50 +0300

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,13 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { in: 6..20 }
+
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
 
