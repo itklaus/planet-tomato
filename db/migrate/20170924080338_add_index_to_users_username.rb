97305e901276622c148219032d0bce03b480ad85
itklaus <it.thomas@yandex.ru>
2017-09-24 11:48:01 +0300

    test

--- db/migrate/20170924080338_add_index_to_users_username.rb
+++ db/migrate/20170924080338_add_index_to_users_username.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersUsername < ActiveRecord::Migration[5.1]
   def change
-    add_index :users, :username, unique: true
+    add_index :users, :username, :email, unique: true
   end
 end
