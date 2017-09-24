1c14aef27ffb6703134ba21f24f25e7e1b705023
itklaus <it.thomas@yandex.ru>
2017-09-24 16:32:06 +0300

    Finish user signup

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @base_title = "Pomodoro Timer"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
 end
